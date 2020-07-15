85910e73c80fa05f0359513368b4781dc21d9205
sw33tr0ll <sweetrollbandit@protonmail.com>
2020-07-15 15:23:59 -0500

    update

--- main.tf
+++ main.tf
@@ -1,2 +1,12 @@
 provider "google" {
+  project     = var.project_id
+  region      = "us-central1"
+}
+
+module "vpc" {
+  source                                 = "./modules/"
+}
+
+module "compute" {
+  source                                 = "./modules/"
 }
